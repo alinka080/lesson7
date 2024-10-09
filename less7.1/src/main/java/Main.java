9abbc6df2dd1c554341400f1fcab1f239bb36198
alinka080 <alinalis080@gmail.com>
2024-10-09 12:14:34 +0300

    Add files via upload

---
+++ less7.1/src/main/java/Main.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+class Ball { }      // Класс "мяч"
+class Accordion { } // Класс "аккордеон"
+class Hat { }       // Класс "шляпа"
+class Umbrella { }  // Класс "зонт"
+
+public class Main {
+    static LostAndFoundOffice lostAndFound = new LostAndFoundOffice();
+
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Accordion accordion = new Accordion();
+        Umbrella umbrella = new Umbrella();
+        Hat hat = new Hat();
+
+        // Сдаём в бюро находок найденные вещи
+        lostAndFound.put(ball);
+        lostAndFound.put(accordion);
+        lostAndFound.put(umbrella);
+
+        checkObject(accordion, "Aккордеон");
+        checkObject(ball, "Мяч");
+        checkObject(null, "Пустая ссылка");
+        checkObject(umbrella, "Зонт");
+        checkObject(hat, "Шляпа");
+    }
+
+    private static void checkObject(Object object, String description) {
+        if(lostAndFound.check(object)) {
+            System.out.println('\'' + description + "' нашёлся!");
+        } else {
+            System.out.println('\'' + description + "' в бюро находок никто не приносил :(");
+        }
+    }
+}
+class LostAndFoundOffice {
+    ArrayList<Object> things = new ArrayList<>();// создайте список things
+
+    public void put(Object element) { // реализуйте метод put()
+        if (element == null) {
+            things.add(element);
+        }
+    }
+
+    public boolean check(Object target) {
+        if (target == null) {
+            return false;
+        }
+        // реализуйте метод check()
+
+        for (Object object : things) { // логика проверки вещи в списке
+            if (object.equals(target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
